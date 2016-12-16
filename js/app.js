0c256d9982b71653753872dbb3f4ab713984513f
Aukai Tirrell <mtirrell@hawaii.edu>
2016-12-16 13:14:07 -1000

    trying to display planet names through use of functions

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 /*jshint esversion: 6*/
-
+const firstUl = document.getElementById('filmList');
 //first person request showing name and homeworld
 function personRequestOne() {
   let personObject = JSON.parse(this.responseText);
@@ -27,23 +27,39 @@ function personRequestTwo() {
 //film request listing each film element and film title
 function filmRequest() {
   let filmArray = JSON.parse(this.responseText).results;
-  for(let i = 0; i < filmArray.length; i++){
-    createList(filmArray,i);
-    for(let j = 0; j < filmArray[i].planets.length; j++){
 
+  for(let i = 0; i < filmArray.length; i++){
+    let planetsUl = document.createElement('ul');
+    createList(filmArray, i, planetsUl);
+    for(let j = 0; j < filmArray[i].planets.length; j++){
+        requestHelper(filmArray[i].planets[j], planetRequest(planetsUl));
+        console.log(planetRequest(planetsUl));
     }
+  }
+  function planetRequest(list) {
+    let planetObject = JSON.parse(this.responseText);
+    console.log(planetObject)
+    let planetNameList = document.createElement('li');
+    let planetNameHeader = document.createElement('h4');
+    planetNameHeader.innerHTML = planetObject.name;
+    planetNameList.appendChild(planetNameHeader);
+    list.appendChild(planetNameList);
+    return planetObject;
+
   }
 }
 
-function createList(array, index) {
-  let list = document.createElement('li');
-  let header = document.createElement('h2');
+function createList(array, index, list) {
+  let filmlist = document.createElement('li');
+  let filmheader = document.createElement('h2');
   let planetHeader = document.createElement('h3');
   planetHeader.innerHTML = "Planets";
-  header.innerHTML = array[index].title;
-  list.appendChild(header);
-  list.appendChild(planetHeader);
-  document.getElementById('filmList').appendChild(list);
+  filmheader.innerHTML = array[index].title;
+  filmlist.appendChild(filmheader);
+  filmlist.appendChild(planetHeader);
+  filmlist.appendChild(list);
+  firstUl.appendChild(filmlist);
+
 }
 
 function requestHelper(link, listener) {
